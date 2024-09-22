1bf6aa5d4642d40804757c8475ba7a6efb4dcd7a
TUN <160473512+LDTU@users.noreply.github.com>
2024-09-22 11:39:41 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Button from './Components/button';
+import Button from './button';
 
 function App() {
   const [input, setInput] = useState(''); 
